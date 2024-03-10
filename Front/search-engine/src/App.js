0fc777dd0a2af2fea03bb37377e650c7428173ae
Karim-Agami <Karim.mohamed702.km@gmail.com>
2024-03-10 10:26:32 +0200

    Basic FrontPage for the Website

--- Front/search-engine/src/App.js
+++ Front/search-engine/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Your search box with placeholder and search icon */}
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search Here"
+            className="search-input"
+          />
+          {/* You can add a simple search icon or customize it later */}
+          <span className="search-icon">🔍</span>
+        </div>
       </header>
     </div>
   );
